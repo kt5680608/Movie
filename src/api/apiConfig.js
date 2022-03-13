ed4c173af0e2d0394c7d04f2d9f08690fa94635f
kt5680608 <gns7893@naver.com>
2022-03-13 16:23:34 +0900

    .env add

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,7 @@
+const API_DOMAIN = process.env.REACT_APP_API_KEY;
 const apiConfig = {
     baseUrl: "https://api.themoviedb.org/3/",
-    apiKey: "419210bbe8ce570ae08cc4023c6a015a",
+    apiKey: `${API_DOMAIN}`,
     originalImage: (imgPath) =>
         `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
